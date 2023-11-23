ffe1c132180e3b1ac3b3fee60ac11ee390a01cfc
Godoyysz <jgfuso@gmail.com>
2023-11-23 14:27:50 -0400

    commit final para 30/11

--- app.js
+++ app.js
@@ -7,16 +7,25 @@ app.use(express.static('public'));
 
 //proxima aula 26.10
 const produtos = [
-  {id: 1, nome: "nome do produto", preco: "30,00", descricao: "descricao do produto", imagem: "meia1.webp"},
-  {id: 2, nome: "nome do produto", preco: "30,00", descricao: "descricao do produto", imagem: "meia2.jpg"},
-  {id: 3, nome: "nome do produto", preco: "30,00", descricao: "descricao do produto", imagem: "meia3.jpg"},
-  {id: 4, nome: "nome do produto", preco: "30,00", descricao: "descricao do produto", imagem: "meia4.jpg"},
-  {id: 5, nome: "nome do produto", preco: "30,00", descricao: "descricao do produto", imagem: "meia5.jpg"},
-  {id: 6, nome: "nome do produto", preco: "30,00", descricao: "descricao do produto", imagem: "meia6.jpg"},
-  {id: 7, nome: "nome do produto", preco: "30,00", descricao: "descricao do produto", imagem: "meia7.jpg"},
-  {id: 8, nome: "nome do produto", preco: "30,00", descricao: "descricao do produto", imagem: "meia8.jpg"},
-  {id: 9, nome: "nome do produto", preco: "30,00", descricao: "descricao do produto", imagem: "meia9.jpg"},
-  {id: 10, nome: "nome do produto", preco: "30,00", descricao: "descricao do produto", imagem: "meia10.jpg"}
+  {id: 1, nome: "meia de mãozinha", preco: "20,00", descricao: "Meia de mãozinha dada por R$20,00", imagem: "meia1.webp"},
+
+  {id: 2, nome: "meia stranger things", preco: "15,00", descricao: "Meia do HellFire Stranger Things por R$15,00", imagem: "meia2.jpg"},
+  
+  {id: 3, nome: "meia de cerveja", preco: "14,50", descricao: "Meia de cerveja por R$14,50", imagem: "meia3.jpg"},
+
+  {id: 4, nome: "meia hamburguer", preco: "23,90", descricao: "Meia de hamburguers por R$23,90", imagem: "meia4.jpg"},
+
+  {id: 5, nome: "meia halloween", preco: "21,80", descricao: "Meia de halloween por R$21,80", imagem: "meia5.jpg"},
+
+  {id: 6, nome: "meia de camera", preco: "17,99", descricao: "Meia de camera fotografica por R$17,99", imagem: "meia6.jpg"},
+
+  {id: 7, nome: "meia de miau", preco: "30,00", descricao: "Meia de gatinhos por R$30,00", imagem: "meia7.jpg"},
+
+  {id: 8, nome: "meia de batata", preco: "10,00", descricao: "Meia de batatas fritas por R$10,00", imagem: "meia8.jpg"},
+
+  {id: 9, nome: "meia da noite estrelada", preco: "18,30", descricao: "Meia de noite estrelada pintura de Van Gogh por R$18,30", imagem: "meia9.jpg"},
+
+  {id: 10, nome: "meia da monalisa", preco: "19,90", descricao: "Meia da Monalisa por R$19,90", imagem: "meia10.jpg"}
 ]
 
 function buscarProdutoPorId(id){
